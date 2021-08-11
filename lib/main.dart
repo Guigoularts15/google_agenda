1fc9a8463e408bf0cb9cb502bf353f3792305bed
Guilherme Goulart <guigoularts@gmail.com>
2021-08-11 19:46:01 -0300

    Edicao final dos arquivos

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_agenda/screens/home/home.dart';
+import 'package:google_agenda/style.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(
+      floatingActionButtonTheme:
+          FloatingActionButtonThemeData(backgroundColor: redTheme),
+      dividerTheme: DividerThemeData(
+        color: grayDivider,
+        thickness: 1,
+      ),
+      primarySwatch: blueTheme,
+    ),
+    debugShowCheckedModeBanner: false,
+  ));
+}
---
+++ lib/provider/list_of_contacts.dart
@@ -0,0 +1,53 @@
+import 'package:google_agenda/models/contact.dart';
+
+List<Contact> listOfContacts = [
+  Contact(
+    name: "Nicole Costa",
+    photo: "assets/images/Nicole.jpg",
+    email: "nicole.costa@gmail.com",
+    phone: "+55 51 5151515151",
+  ),
+  Contact(
+    name: "Camila Santos",
+    photo: "assets/images/Camila.jpg",
+    email: "camila.santos@gmail.com",
+    phone: "+55 51 5151515151",
+  ),
+  Contact(
+    name: "Claudia Oliveira",
+    photo: "assets/images/Claudia.jpg",
+    email: "claudia.oliveira@gmail.com",
+    phone: "+55 51 5151515151",
+  ),
+  Contact(
+    name: "Juliana Goulart",
+    photo: "assets/images/Juliana.jpg",
+    email: "juliana.goulart@gmail.com",
+    phone: "+55 51 5151515151",
+  ),
+  Contact(
+    name: "Lucas Gama",
+    photo: "assets/images/Lucas.jpg",
+    email: "lucas.gama@gmail.com",
+    phone: "+55 51 5151515151",
+  ),
+  Contact(
+    name: "Marina Macedo",
+    photo: "assets/images/Marina.jpg",
+    email: "marina.macedo@gmail.com",
+    phone: "+55 51 5151515151",
+    isFavorite: true,
+  ),
+  Contact(
+    name: "Paulo Edivandro",
+    photo: "assets/images/Paulo.jpg",
+    email: "paulo.edivandro@gmail.com",
+    phone: "+55 51 5151515151",
+  ),
+  Contact(
+    name: "Thomas Tobias",
+    photo: "assets/images/Thomas.jpg",
+    email: "thomas.tobias@gmail.com",
+    phone: "+55 51 5151515151",
+  ),
+];
